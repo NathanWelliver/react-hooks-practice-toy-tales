912527d06c6d9665f92d436d7616cf65d6cc7181
Nathan Welliver <nwelliver.564@gmail.com>
2024-05-28 18:48:48 -0400

    complete lab

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,41 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, setToys}) {
+
+  function handleUpdate(id, likes){
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "likes": likes
+      })
+    })
+    .then((r) => r.json())
+    .then((updatedToy) => {
+      const updatedToys = toys.map((toy) => 
+        toy.id === id ? {...toy, likes: updatedToy.likes}: toy
+    );
+    setToys(updatedToys)
+    });
+  }
+
+  function deleteToy(deletedToy){
+    const updatedToys = toys.filter((toy) => toy.id !== deletedToy);
+    setToys(updatedToys)
+  }
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toys.map((toy) => (
+      <ToyCard 
+        key={toy.id}
+        toy={toy}
+        onDeleteToy={deleteToy}
+        onUpdate={handleUpdate}
+      />
+    ))}</div>
   );
 }
 
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({setToys}) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+  })
+
+  function handleChange(e){
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    const newToy = {
+      name: formData.name,
+      image: formData.image
+    };
+
+    fetch("http://localhost:3001/toys", {
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then((r) => r.json())
+    .then((addedToy) => {
+      setToys((toys) => [...toys, addedToy])
+    });
+  };
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
@@ -17,6 +51,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
